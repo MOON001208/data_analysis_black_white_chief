029a40536c3e2b27d5a242a6c53b5d1905568aaf
Seoha <68509963+Jeong-Seoha@users.noreply.github.com>
2026-01-14 13:29:51 +0900

    [modify] test

--- dags/test.py
+++ dags/test.py
@@ -1,21 +1,10 @@
 from airflow import DAG
-from airflow.operators.python import PythonOperator
-import pendulum
-import datetime
+from datetime import datetime
 
-# DAG 정의
 with DAG(
     dag_id="test",
     schedule="@daily",
     start_date=datetime(2024, 1, 1),
+    catchup=False,
 ):
-
-    def print_execution_status():
-        current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        print(f"[{current_time}] DAG가 정상적으로 실행되었습니다.")
-
-    # 실행 확인 태스크
-    check_task = PythonOperator(
-        task_id='check_execution_task',
-        python_callable=print_execution_status
-    )
+    pass
